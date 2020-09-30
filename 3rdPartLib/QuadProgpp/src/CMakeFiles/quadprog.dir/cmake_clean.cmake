170cdba242a9b043049ce2a9cdaf81bf6e47d9db
Jeremy Chang <37570145+st88018@users.noreply.github.com>
2020-09-30 21:42:12 +0800

    0930

---
+++ 3rdPartLib/QuadProgpp/src/CMakeFiles/quadprog.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quadprog.dir/Array.cc.o"
+  "CMakeFiles/quadprog.dir/QuadProg++.cc.o"
+  "libquadprog.pdb"
+  "libquadprog.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quadprog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
